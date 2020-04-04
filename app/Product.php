43be540d25f394dd6b877fdef021bc90c63aa1ca
AhmedAdly20 <ahmedadly132@gmail.com>
2020-04-05 00:10:16 +0200

    Categories and Pagenation

--- app/Product.php
+++ app/Product.php
@@ -10,4 +10,9 @@ class Product extends Model
     {
         return $query->inRandomOrder()->take(4);
     }
+
+    public function categories()
+    {
+        return $this->belongsToMany('App\Category');
+    }
 }
